0adc927d34d91c39a5d21923b77a43097586347d
El Dainosor <eldainosor@gmail.com>
2023-01-02 17:33:19 -0300

    clockfont: bring up initial fonts
    after r16, the lockscreen clock font became overlayable in a different way and more friendly with RRO overlays.
    let's take this chance and allow the user to use a font different than our default one.
    
    Change-Id: Ia663c69273e74d093d6d6a24745f5ebd8427ebed
    Signed-off-by: aswin7469 <aswinas@pixysos.com>

--- config/rro_overlays.mk
+++ config/rro_overlays.mk
@@ -3,3 +3,19 @@ PRODUCT_PACKAGES += \
     CustomPixelLauncherOverlay \
     NetworkStackOverlay \
     ImmersiveNavigationOverlay
+
+# Lockscreen clock fonts
+PRODUCT_PACKAGES += \
+    ClockFontAdventProOverlay \
+    ClockFontBigNoodleTiltingOverlay \
+    ClockFontCherrySwashOverlay \
+    ClockFontHeadlineOverlay \
+    ClockFontRoadRageOverlay \
+    ClockFontSnowstormOverlay \
+    ClockFontViburOverlay \
+    ClockFontAlienLeagueOverlay \
+    ClockFontBikoOverlay \
+    ClockFontGinoraSansOverlay \
+    ClockFontRivieraOverlay \
+    ClockFontSedgwickAveOverlay \
+    ClockFontUnionOverlay
