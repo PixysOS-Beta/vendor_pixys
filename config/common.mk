e9116bc186abc70602bc7b158ba105f8647c2f23
aswin7469 <aswinas@pixysos.com>
2023-12-03 15:20:59 +0530

    aospa face sense
    Signed-off-by: aswin7469 <aswinas@pixysos.com>

--- config/common.mk
+++ config/common.mk
@@ -165,12 +165,16 @@ endif
 TARGET_SUPPORTS_GOOGLE_BATTERY ?= false
 
 # faceunlock
-TARGET_FACE_UNLOCK_SUPPORTED ?= false
+TARGET_FACE_UNLOCK_SUPPORTED ?= true
 ifeq ($(TARGET_FACE_UNLOCK_SUPPORTED),true)
+
 PRODUCT_PACKAGES += \
-    FaceUnlockService
+    ParanoidSense \
+    AOSPASettingsOverlay
+
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     ro.face_unlock_service.enabled=$(TARGET_FACE_UNLOCK_SUPPORTED)
+
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.biometrics.face.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/android.hardware.biometrics.face.xml
 endif
