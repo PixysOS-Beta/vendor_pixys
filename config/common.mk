d881b8393fe644da7e597108d00bb0cf357fc629
aswin7469 <aswinas@pixysos.com>
2023-12-11 06:06:23 +0530

    bruh

--- config/common.mk
+++ config/common.mk
@@ -173,7 +173,7 @@ PRODUCT_PACKAGES += \
     AOSPASettingsOverlay
 
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
-    ro.face_unlock_service.enabled=$(TARGET_FACE_UNLOCK_SUPPORTED)
+    ro.face.sense_service=$(TARGET_FACE_UNLOCK_SUPPORTED)
 
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.biometrics.face.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/android.hardware.biometrics.face.xml
